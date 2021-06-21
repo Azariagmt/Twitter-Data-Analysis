c310fe43c7ff5efe1c1358ff2c5633f140f015fa
Azariagmt <azariagebremichael10@gmail.com>
2021-06-21 21:01:11 +0300

    fixes friends count function

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -70,12 +70,12 @@ class TweetDfExtractor:
 
     def find_followers_count(self) -> list:
         followers_count = [tweet['user']['followers_count']
-                           if 'user' in tweet else ' ' for tweet in self.tweets_list]
+                           if 'user' in tweet else '' for tweet in self.tweets_list]
         return followers_count
 
     def find_friends_count(self) -> list:
-        friends_count = [tweet['friends_count']
-                         if 'friends_count' in tweet else ' ' for tweet in self.tweets_list]
+        friends_count = [tweet['user']['friends_count']
+                         if 'user' in tweet else '' for tweet in self.tweets_list]
         return friends_count
 
     def is_sensitive(self) -> list:
