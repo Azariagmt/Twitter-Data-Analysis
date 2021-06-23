b72ecc37b8ad13c06c922cde12934a15769536d5
Azariagmt <azariagebremichael10@gmail.com>
2021-06-23 22:28:35 +0300

    adds herocku Procfile

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -19,4 +20,5 @@ def sentiments():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0',debug=True, port=port)
